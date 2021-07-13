5612e6ec74ff7a616ebb5228fce01145c92b38d4
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-07-13 23:40:40 +0300

    fix: fix mistakes after 2nd rewiev

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 });
 
 mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
