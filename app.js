a1dc56d08bd0450f05c3f3a5fb958bae7a052061
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-07-29 22:12:06 +0300

    PW14. Ready for second check

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 const mongoose = require("mongoose");
 const { celebrate, Joi, errors } = require("celebrate");
+const helmet = require("helmet");
 
 const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
@@ -12,6 +13,7 @@ const regExp = require("./regexp/regexp");
 const NotFoundError = require("./errors/not-found-err");
 require("dotenv").config();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
